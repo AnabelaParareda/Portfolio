a07205272299280916382848c77a47767511e265
Anabela <anabela_parareda@hotmail.com>
2023-02-21 22:44:37 -0300

    Script y Style

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
 function seleccionar(link) {
     var opciones = document.querySelectorAll('#links a');
     opciones[0].className = "";
@@ -8,12 +7,10 @@ function seleccionar(link) {
     opciones[4].className = "";
     link.className = "seleccionado";
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion en modo responsive
     var x = document.getElementById("nav");
     x.className = "";
 }
 
-//función que muestra el menu responsive
 function responsiveMenu() {
     var x = document.getElementById("nav");
     if (x.className === "") {
@@ -22,8 +19,6 @@ function responsiveMenu() {
         x.className = "";
     }
 }
-
-//animacion en barra de skills
 window.onscroll = function() { efectoHabilidades() };
 
 function efectoHabilidades() {
