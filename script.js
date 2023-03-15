4588829498b0bda96f7f5b0faffc32a5eb4923da
Anabela <anabela_parareda@hotmail.com>
2023-03-15 14:16:51 -0300

    Actualizacion

--- script.js
+++ script.js
@@ -32,3 +32,59 @@ function efectoHabilidades() {
     }
 
 }
+
+/*const contactForm = document.getElementById('contactForm');
+const inputs = document.querySelectorAll('#contactForm input');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+
+const validarcontactForm = (e) => {
+    switch (e.target.name) {
+        case "nombre":
+            if (expresiones.nombre.test(e.target.value)) {
+
+            } else {
+            }
+
+        break;
+        
+        case "email":
+            break;
+    }
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('contacto__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('contacto__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .contacto__input-error`).classList.remove('contacto__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('contacto__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('contacto__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .contacto__input-error`).classList.add('contacto__input-error-activo');
+		campos[campo] = false;
+    } 
+}
+    
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarcontactForm);
+    input.addEventListener('blur', validarcontactForm);
+});
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+
+
+//const nombre = document.getElementById("nombre")
+//const email = document.getElementById("email")
+//onst message = document.getElementById("mensaje") */
